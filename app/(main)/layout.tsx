0860b447675a6a13708b2fb007b4598d8da98de9
bisxxal <bishalkandi859494@gmail.com>
2024-11-20 01:08:21 +0530

    end

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -5,6 +5,7 @@ import Sidebar from '@/components/custom/Sidebar'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
+export const dynamic = "force-dynamic";
 const Mainlayout = async ({children}:{children:React.ReactNode}) => {
     const currentUser = await getCurrentUser(); 
     if (!currentUser) return redirect("/sign-in");
